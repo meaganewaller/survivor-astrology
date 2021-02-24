31cbabc8657f0b09e48ea0b85775dd645fac90fb
Meagan Waller <meaganewaller@gmail.com>
2021-02-23 21:58:41 -0500

    🎨 Adds survivor contestants total and percentage win by sign

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -6,4 +6,20 @@ module HomeHelper
       yield "#{sign[0]} - #{Contestant.where_sign_places(sign[0], 1).count}"
     end
   end
+
+  def by_sign
+    SunSignCalculator::SUN_SIGNS.each do |sign|
+      yield "#{sign[0]} - #{Contestant.where(sun_sign: sign[0]).count}"
+    end
+  end
+
+  def percent_win
+    SunSignCalculator::SUN_SIGNS.each do |sign|
+      yield "#{sign[0]}: #{percentage(sign[0])}"
+    end
+  end
+
+  def percentage(sign)
+    Contestant.where_sign_places(sign, 1).count.to_f / Contestant.where(sun_sign: sign).count.to_f
+  end
 end
